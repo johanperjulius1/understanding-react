4657382b9fb3871839337622c8984f103b9cc8dd
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-05-26 16:14:21 +0200

    starting on gamecontrols

--- vite-test/src/Components/GameControls.jsx
+++ vite-test/src/Components/GameControls.jsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+import styles from "./gamecontrols.module.css";
+
 export default function GameControls() {
-  return <section>Hello from Game controls</section>;
+  const [userInput, setUserInput] = useState("");
+
+  const handleUserInput = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <section>
+      <form method="post">
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleUserInput}
+          placeholder="Type or click"
+        />
+      </form>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button className={styles["hex-button"]}>Y</button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button className={styles["hex-button"]}>D</button>
+          <button className={`${styles["hex-button"]} ${styles["center"]}`}>
+            C
+          </button>
+          <button className={styles["hex-button"]}>A</button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button className={styles["hex-button"]}>R</button>
+          <button className={styles["hex-button"]}>U</button>
+          <button className={styles["hex-button"]}>H</button>
+        </div>
+      </div>
+    </section>
+  );
 }
